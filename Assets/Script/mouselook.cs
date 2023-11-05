c6fa7e55d7d553e464fc7602fdfb04ba89b9356a
Kairui Liang <liangkairuica@163.com>
2023-11-05 14:55:57 -0400

    first peron camera

---
+++ Assets/Script/mouselook.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class mouselook : MonoBehaviour
+{
+    public Transform player;
+    private float mouseX, mouseY;
+    public float mouseSensitivity;
+    public float xRotation;
+
+    private void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -70f, 70f);
+        
+        player.Rotate(Vector3.up * mouseX);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+    }
+}
