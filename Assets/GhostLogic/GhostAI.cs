331b52164da93160fdf6b134b2ca824a57ceedb7
Ziliang Zhu <zl658202@dal.ca>
2023-11-17 13:30:45 -0400

    Enemy AI safespace added and Stone lion added

--- Assets/GhostLogic/GhostAI.cs
+++ Assets/GhostLogic/GhostAI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,16 +10,20 @@ public class GhostAI : MonoBehaviour
     // Start is called before the first frame update
     public Transform player;
     public GhostPath path;
-    public float chaseDis = 100.0f;
+    public float chaseDis = 60.0f;
+    public float patrolSpeed = 5.46f;
+    public float chasingSpeed = 7.5f;
+    public Collider safesapce;
     private Transform[] waypoints;
     private int currentWaypoint;
     private NavMeshAgent nav;
+    private Boolean chasing;
 
 
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
-
+        chasing = false;
   
         if (path != null)
         {
@@ -28,6 +33,7 @@ public class GhostAI : MonoBehaviour
             {
                 currentWaypoint = 0;
                 nav.enabled = true;
+                nav.speed = patrolSpeed;
                 nav.SetDestination(waypoints[currentWaypoint].position);
             }
             else
@@ -44,31 +50,62 @@ public class GhostAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float playerDis = Vector3.Distance(transform.position, player.position);
+        float disFromPlayer = Vector3.Distance(transform.position, player.position);
 
-        if (playerDis < chaseDis)
+        if (chasing == true)
         {
-            nav.SetDestination(player.position);
+            if(disFromPlayer > chaseDis || safesapce.bounds.Contains(player.position))
+            {
+                chasing = false;
+                FindNearestWaypoint();
+                nav.SetDestination(waypoints[currentWaypoint].position);
+            }
+            else
+            {
+                nav.SetDestination(player.position);
+                nav.speed = chasingSpeed;
+            }
         }
-
         else
         {
-            if (!nav.pathPending && nav.remainingDistance < 0.5f)
+            if(disFromPlayer < chaseDis && !safesapce.bounds.Contains(player.position))
             {
-                nextWaypoint();
+                chasing = true;
+                nav.SetDestination(player.position);
+                nav.speed = chasingSpeed;
             }
-            //else if(!nav.pathPending && nav.remainingDistance >= 0.5f)
-            //{
-            //    var sortedWaypoints = waypoints.OrderBy(dis => Vector3.Distance(transform.position, dis.position)).ToArray();
-            //    nav.SetDestination(sortedWaypoints[0].position);
-            //}
+            else
+            {
+                float disFromWaypoint = Vector3.Distance(transform.position, waypoints[currentWaypoint].position);
+                Debug.Log("This is right. BUt the distance is " + disFromWaypoint);
+                if ( !nav.pathPending && disFromWaypoint < 3.0f)
+                {
+                    NextWaypoint();
+                }
+            }
         }
     }
 
-    void nextWaypoint()
+    void NextWaypoint()
     {
+        currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
         nav.SetDestination(waypoints[currentWaypoint].position);
-        currentWaypoint = (currentWaypoint + 1) % waypoints.Length; 
+        Debug.Log(currentWaypoint);
     }
 
+    void FindNearestWaypoint()
+    {
+        int shortestIndex = 0;
+        float temp = Vector3.Distance(transform.position, waypoints[shortestIndex].position);
+        for(int i = 1; i < waypoints.Length; i++)
+        {
+            if(temp >= Vector3.Distance(transform.position, waypoints[i].position))
+            {
+                temp = Vector3.Distance(transform.position, waypoints[i].position);
+                shortestIndex = i;
+            }
+        }
+
+        currentWaypoint = shortestIndex; 
+    }
 }
