156f6b3707f93706234a642462d8c35c12c11eda
oxner <luke.oxner@dal.ca>
2023-11-18 13:36:38 -0400

    Updated with all branches, changed the level/scene structure, added a menu screen, and added movement to the character/camera in the first two levels

--- Assets/Script/mouselook.cs
+++ Assets/Script/mouselook.cs
@@ -7,33 +7,40 @@ using UnityEngine;
 
 public class mouselook : MonoBehaviour
 {
+    public float sensX;
+    public float sensY;
+    public float playerHeight = 2f; // Adjust based on character's height
+    public Transform orientation;
     public Transform player;
-    private float mouseX, mouseY;
-    public float mouseSensitivity = 200f;
-    public float xRotation;
+
+    float xRotation;
+    float yRotation;
 
     public void Start()
     {
-        
-            player = GameObject.Find("maincharacter").transform;
-            if (player == null)
-            {
-                Debug.LogError("no player component found.");  
-            }
-        
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void Update()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        // Get mouse input
+        float mouseX = Input.GetAxis("Mouse X")  * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensY;
 
+        yRotation += mouseX;
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -70f, 70f);
+        xRotation = Mathf.Clamp(xRotation, -60f, 60f);
         
-        player.Rotate(Vector3.up * mouseX);
-        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        // Rotate the camera and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+        // Rotate the character
+        player.rotation = Quaternion.Euler(0,yRotation, 0);
+
+        // Adjust the camera position to avoid clipping through the character
+        Vector3 playerHeadPosition = player.position + Vector3.up * playerHeight;
+        transform.position = playerHeadPosition + transform.forward * 0.75f; // Adjust the multiplier if needed
     }
 }
