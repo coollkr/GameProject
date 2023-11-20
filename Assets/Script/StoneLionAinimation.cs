d9266d154cf55bd336119ce1bd306fff47ed3698
zheyu Gu <zh987373@dal.ca>
2023-11-20 13:22:57 -0400

    update puzzle and fix AI logic

--- Assets/Script/StoneLionAinimation.cs
+++ Assets/Script/StoneLionAinimation.cs
@@ -4,9 +4,7 @@ using System.Collections;
 public class RotateStoneLion : MonoBehaviour
 {
     private bool isPlayerNear = false;
-    // 旋转的目标角度
     public float totalRotation =0f;
-    // 旋转速度
     public float rotationSpeed = 1f;
 
     private bool isRotating = false;
@@ -32,7 +30,6 @@ public class RotateStoneLion : MonoBehaviour
     {
         if (isPlayerNear && Input.GetKeyDown(KeyCode.R)&& !isRotating)
         {
-            // 启动旋转动画
             StartCoroutine(RotateObject());
         }
     }
@@ -41,13 +38,10 @@ public class RotateStoneLion : MonoBehaviour
     {
         isRotating = true;
 
-        // 获取当前物体的旋转
         Quaternion startRotation = stoneLion.transform.rotation;
-        // 计算目标旋转
         float targetRotation = totalRotation + 90f;
         Quaternion targetRotationQuaternion = Quaternion.Euler(0, targetRotation, 0);
 
-        // 插值旋转
         float elapsedTime = 0f;
         while (elapsedTime < 1f)
         {
@@ -56,9 +50,8 @@ public class RotateStoneLion : MonoBehaviour
             yield return null;
         }
 
-        // 确保物体准确达到目标角度
         stoneLion.transform.rotation = targetRotationQuaternion;
-        totalRotation = targetRotation % 360f; // 确保总旋转角度不超过360度
+        totalRotation = targetRotation % 360f; 
 
         isRotating = false;
     }
