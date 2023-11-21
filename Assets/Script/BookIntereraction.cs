e4eec602df3f52e9433267de2f5ca4b0fbe2ae0a
zheyu Gu <zh987373@dal.ca>
2023-11-21 18:02:07 -0400

    change lighing mode logic

--- Assets/Script/BookIntereraction.cs
+++ Assets/Script/BookIntereraction.cs
@@ -3,18 +3,18 @@ using TMPro;
 
 public class BookInteraction : MonoBehaviour
 {
-    public GameObject greyBackgroundCanvas; // 整个Canvas
-    public TMP_Text bookText; // 显示书本内容的TextMeshPro对象
-    public GameObject openBookPrompt; // “Press R to Open the Book”的提示
-    public GameObject closeBookPrompt; // “Press R to Close the Book”的提示
-    public Collider bookCollider; // 书本的Collider
+    public GameObject greyBackgroundCanvas; 
+    public TMP_Text bookText; 
+    public GameObject openBookPrompt; 
+    public GameObject closeBookPrompt; 
+    public Collider bookCollider; 
 
     private bool isPlayerNear = false;
     private bool isBookOpen = false;
 
     void Start()
     {
-        greyBackgroundCanvas.SetActive(false); // 确保开始时整个Canvas是隐藏的
+        greyBackgroundCanvas.SetActive(false); 
     }
 
     void Update()
@@ -30,7 +30,7 @@ private void OnTriggerEnter(Collider other)
     if (other.CompareTag("Player"))
     {
         isPlayerNear = true;
-        openBookPrompt.SetActive(true); // 只显示打开书本的提示
+        openBookPrompt.SetActive(true); 
     }
 }
 
@@ -39,26 +39,26 @@ private void OnTriggerExit(Collider other)
     if (other.CompareTag("Player"))
     {
         isPlayerNear = false;
-        CloseBook(); // 当玩家离开时关闭书本
+        CloseBook(); 
     }
 }
 
 void ToggleBookUI()
 {
     isBookOpen = !isBookOpen;
-    greyBackgroundCanvas.SetActive(isBookOpen); // 控制整个Canvas的显示和隐藏
-    bookText.gameObject.SetActive(isBookOpen); // 控制书本内容的显示和隐藏
-    closeBookPrompt.SetActive(isBookOpen); // 控制关闭书本的提示显示和隐藏
-    openBookPrompt.SetActive(!isBookOpen); // 控制打开书本的提示显示和隐藏
+    greyBackgroundCanvas.SetActive(isBookOpen); 
+    bookText.gameObject.SetActive(isBookOpen); 
+    closeBookPrompt.SetActive(isBookOpen); 
+    openBookPrompt.SetActive(!isBookOpen); 
 }
 
 public void CloseBook()
 {
     isBookOpen = false;
-    greyBackgroundCanvas.SetActive(false); // 隐藏整个Canvas
-    bookText.gameObject.SetActive(false); // 隐藏书本内容
-    closeBookPrompt.SetActive(false); // 隐藏关闭书本的提示
-    openBookPrompt.SetActive(isPlayerNear); // 如果玩家仍然靠近，则显示打开书本的提示
+    greyBackgroundCanvas.SetActive(false); 
+    bookText.gameObject.SetActive(false); 
+    closeBookPrompt.SetActive(false); 
+    openBookPrompt.SetActive(isPlayerNear); 
 }
 
 }
--- Assets/Script/DoorPuzzleVoice.cs
+++ Assets/Script/DoorPuzzleVoice.cs
@@ -6,23 +6,23 @@ using System.Collections.Generic;
 
 public class DoorPuzzleVoice : MonoBehaviour
 {
-    public GameObject uiTextObject; // R 的 UI 文本对象
-    public AudioClip[] knockingSounds; // 预先录制好的敲门声音数组
-    private AudioSource audioSource; // 柜子的 AudioSource 组件
-    private int knockSequenceIndex = 0; // 当前敲门声音序列索引
+    public GameObject uiTextObject; 
+    public AudioClip[] knockingSounds; 
+    private AudioSource audioSource; 
+    private int knockSequenceIndex = 0; 
     public List<GameObject> lights;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        uiTextObject.SetActive(false); // 初始时隐藏 UI
+        uiTextObject.SetActive(false); 
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && knockSequenceIndex < knockingSounds.Length)
         {
-            uiTextObject.SetActive(true); // 显示 R 的 UI
+            uiTextObject.SetActive(true); 
         }
     }
 
@@ -30,7 +30,7 @@ public class DoorPuzzleVoice : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            uiTextObject.SetActive(false); // 隐藏 R 的 UI
+            uiTextObject.SetActive(false); 
         }
     }
 
@@ -38,7 +38,7 @@ public class DoorPuzzleVoice : MonoBehaviour
     {
         if (uiTextObject.activeSelf && Input.GetKeyDown(KeyCode.R))
         {
-            uiTextObject.SetActive(false); // 隐藏 R 的 UI
+            uiTextObject.SetActive(false);
             PlayKnockingSound();
         }
     }
@@ -55,25 +55,47 @@ public class DoorPuzzleVoice : MonoBehaviour
     }
 
     IEnumerator ShowUIAfterSound(float delay)
+    {
+        if (knockSequenceIndex == 4) // Fourth knock.
         {
-            yield return new WaitForSeconds(delay);
-            if (knockSequenceIndex < knockingSounds.Length)
+            yield return new WaitForSeconds(3); // Wait until the recording reaches the 3rd second
+
+            // For the next 4 seconds, perform a random on/off operation for each light
+            float endTime = Time.time + 4;
+            while (Time.time < endTime)
             {
-                uiTextObject.SetActive(true); // 声音播放完毕后再次显示 R 的 UI
+                foreach (var light in lights)
+                {
+                    light.SetActive(Random.Range(0, 2) > 0); // Randomize the state of each light
+                }
+                yield return new WaitForSeconds(0.1f); // wait
             }
-            else if (knockSequenceIndex == 4) // 检查是否是第四次敲门
-            {
-                ToggleLights(false); // 关闭所有灯
-                yield return new WaitForSeconds(5); // 等待五秒
-                ToggleLights(true); // 重新打开所有灯
-            }
+
+            yield return new WaitForSeconds(delay - 7); // Waiting for the remaining recording playback time
+        }
+        else
+        {
+            yield return new WaitForSeconds(delay); // Normal wait in other cases
         }
 
-        void ToggleLights(bool state)
+        if (knockSequenceIndex < knockingSounds.Length)
         {
-            foreach (var light in lights)
-            {
-                light.SetActive(state);
-            }
+            uiTextObject.SetActive(true); // The UI of R is displayed again after the sound is played.
         }
+        else
+        {
+            ToggleLights(false); // Turn off all lights.
+            yield return new WaitForSeconds(5); // Wait five seconds.
+            ToggleLights(true); // Turn all the lights back on.
+        }
+    }
+
+    void ToggleLights(bool state)
+    {
+        foreach (var light in lights)
+        {
+            light.SetActive(state);
+        }
+    }
+
 }
