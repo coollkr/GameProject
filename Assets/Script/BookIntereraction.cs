034f751d96191a0ac3486d02af6f187293b74296
zheyu Gu <zh987373@dal.ca>
2023-11-26 13:41:54 -0400

    change details for the demo record

--- Assets/Script/BookIntereraction.cs
+++ Assets/Script/BookIntereraction.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class BookInteraction : MonoBehaviour
 {
@@ -14,7 +15,9 @@ public class BookInteraction : MonoBehaviour
 
     void Start()
     {
-        greyBackgroundCanvas.SetActive(false); 
+        greyBackgroundCanvas.SetActive(false);
+        openBookPrompt.SetActive(false);
+        closeBookPrompt.SetActive(false);
     }
 
     void Update()
@@ -25,40 +28,82 @@ public class BookInteraction : MonoBehaviour
         }
     }
 
-private void OnTriggerEnter(Collider other)
-{
-    if (other.CompareTag("Player"))
+    private void OnTriggerEnter(Collider other)
     {
-        isPlayerNear = true;
-        openBookPrompt.SetActive(true); 
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = true;
+            // Starts the incremental OpenBookPrompt concatenation.
+            StartCoroutine(FadeInText(openBookPrompt, "Press R to Open Book", 1f)); 
+        }
     }
-}
 
-private void OnTriggerExit(Collider other)
-{
-    if (other.CompareTag("Player"))
+    private void OnTriggerExit(Collider other)
     {
-        isPlayerNear = false;
-        CloseBook(); 
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = false;
+            CloseBook();
+        }
     }
-}
 
-void ToggleBookUI()
-{
-    isBookOpen = !isBookOpen;
-    greyBackgroundCanvas.SetActive(isBookOpen); 
-    bookText.gameObject.SetActive(isBookOpen); 
-    closeBookPrompt.SetActive(isBookOpen); 
-    openBookPrompt.SetActive(!isBookOpen); 
-}
+    void ToggleBookUI()
+    {
+        isBookOpen = !isBookOpen;
+        greyBackgroundCanvas.SetActive(isBookOpen);
+        if (isBookOpen)
+        {
+            StartCoroutine(FadeInTextByLine(bookText, 0.5f)); 
+        }
+        else
+        {
+            bookText.gameObject.SetActive(false);
+        }
+        closeBookPrompt.SetActive(isBookOpen);
+        openBookPrompt.SetActive(!isBookOpen);
+    }
 
-public void CloseBook()
-{
-    isBookOpen = false;
-    greyBackgroundCanvas.SetActive(false); 
-    bookText.gameObject.SetActive(false); 
-    closeBookPrompt.SetActive(false); 
-    openBookPrompt.SetActive(isPlayerNear); 
-}
+    public void CloseBook()
+    {
+        isBookOpen = false;
+        greyBackgroundCanvas.SetActive(false);
+        bookText.gameObject.SetActive(false);
+        closeBookPrompt.SetActive(false);
+        openBookPrompt.SetActive(isPlayerNear);
+    }
+
+    IEnumerator FadeInText(GameObject promptObject, string fullText, float duration)
+    {
+        promptObject.SetActive(true); 
+        TMP_Text textComponent = promptObject.GetComponent<TMP_Text>();
+        if (textComponent != null)
+        {
+            textComponent.text = "";
+            float timePerCharacter = duration / fullText.Length;
+            foreach (char c in fullText)
+            {
+                textComponent.text += c;
+                yield return new WaitForSeconds(timePerCharacter);
+            }
+        }
+        else
+        {
+            Debug.LogError("TMP_Text component not found on the object!");
+        }
+    }
+
+    IEnumerator FadeInTextByLine(TMP_Text textComponent, float timePerLine)
+    {
+        textComponent.gameObject.SetActive(true);
+        string fullText = textComponent.text; 
+        textComponent.text = "";
+        string[] lines = fullText.Split('\n');
+
+        foreach (string line in lines)
+        {
+            textComponent.text += line + "\n";
+            yield return new WaitForSeconds(timePerLine); 
+        }
+    }
 
 }
